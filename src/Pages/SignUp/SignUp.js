d4d32cd68543f4008de5b50064ced3b61aad8b9e
pmb087 <pmb087@gmail.com>
2022-10-03 14:42:26 +0900

    initial commit

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div className="signUp">
+      <span className="signUpSpan">회원가입 입니다.</span>
+    </div>
+  );
+}
+
+export default SignUp;
